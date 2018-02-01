afab65ce8228078e3b143fa26b32b6ff48a45f5f
lekkerbit <lekkerbit@gmail.com>
2018-02-01 23:06:07 +0800

    timer implemented

--- os/kernel/src/kmain.rs
+++ os/kernel/src/kmain.rs
@@ -3,6 +3,7 @@
 #![feature(const_fn)]
 #![feature(asm)]
 #![feature(optin_builtin_traits)]
+#![feature(compiler_builtins_lib, lang_items, asm, pointer_methods)]
 #![feature(decl_macro)]
 #![feature(repr_align)]
 #![feature(attr_literals)]
@@ -17,7 +18,28 @@ pub mod mutex;
 pub mod console;
 pub mod shell;
 
+
+pub fn blinky() {
+    const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+    
+    const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+    const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+    const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
+    // STEP 1: Set GPIO Pin 16 as output.
+    unsafe {GPIO_FSEL1.write_volatile(0b001 << 18);}
+    // STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        unsafe {GPIO_SET0.write_volatile(1 << 16)};
+        spin_sleep_ms(1000);
+        unsafe {GPIO_CLR0.write_volatile(1 << 16)};
+        spin_sleep_ms(1000);
+    }
+}
+
+use pi::timer::spin_sleep_ms;
 #[no_mangle]
 pub extern "C" fn kmain() {
+    blinky();
     // FIXME: Start the shell.
 }
