69dea9e195a3f8f549fe90baa75147364e92ab76
lekkerbit <lekkerbit@gmail.com>
2018-02-02 01:18:16 +0800

    gpio driver implemented

--- os/kernel/src/kmain.rs
+++ os/kernel/src/kmain.rs
@@ -20,19 +20,11 @@ pub mod shell;
 
 
 pub fn blinky() {
-    const GPIO_BASE: usize = 0x3F000000 + 0x200000;
-    
-    const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
-    const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
-    const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
-
-    // STEP 1: Set GPIO Pin 16 as output.
-    unsafe {GPIO_FSEL1.write_volatile(0b001 << 18);}
-    // STEP 2: Continuously set and clear GPIO 16.
+    let mut gpio16 = pi::gpio::Gpio::new(16).into_output();
     loop {
-        unsafe {GPIO_SET0.write_volatile(1 << 16)};
+        gpio16.set();
         spin_sleep_ms(1000);
-        unsafe {GPIO_CLR0.write_volatile(1 << 16)};
+        gpio16.clear();
         spin_sleep_ms(1000);
     }
 }
